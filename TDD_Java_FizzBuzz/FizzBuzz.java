7076efff9946c280d963839a24f1e3ae1610334e
Chenbo Yi <yichenbo@outlook.com>
2019-05-27 21:47:59 +0100

    unit test FizzBuzz in Java by using Junit

--- TDD_Java_FizzBuzz/FizzBuzz.java
+++ TDD_Java_FizzBuzz/FizzBuzz.java
@@ -1,5 +1,13 @@
 public class FizzBuzz{
    static String fizzbuzz(int number){
-      return Integer.toString(number);
+     if(number%3==0 && number%5 ==0){
+       return "FizzBuzz";
+     }else if(number%3==0){
+       return "Fizz";
+     }else if(number%5==0){
+       return "Buzz";
+     }else{
+       return Integer.toString(number);
+     }
     }
   }
--- TDD_Java_FizzBuzz/TestFizzBuzz.java
+++ TDD_Java_FizzBuzz/TestFizzBuzz.java
@@ -13,4 +13,34 @@ public void testPrintsNumber2() {
     assertEquals("2", FizzBuzz.fizzbuzz(2));
     }
 
+@Test
+public void testPrintsNumberFizzwhen3() {
+    assertEquals("Fizz", FizzBuzz.fizzbuzz(3));
+    }
+
+@Test
+public void testPrintsNumberFizzwhen6() {
+    assertEquals("Fizz", FizzBuzz.fizzbuzz(6));
+    }
+
+@Test
+public void testPrintsNumberBuzzwhen5() {
+    assertEquals("Buzz", FizzBuzz.fizzbuzz(5));
+    }
+
+@Test
+public void testPrintsNumberBuzzwhen10() {
+    assertEquals("Buzz", FizzBuzz.fizzbuzz(10));
+    }
+
+@Test
+public void testPrintsNumberFizzBuzzwhen15() {
+    assertEquals("FizzBuzz", FizzBuzz.fizzbuzz(15));
+    }
+
+@Test
+public void testPrintsNumberFizzBuzzwhen30() {
+    assertEquals("FizzBuzz", FizzBuzz.fizzbuzz(30));
+    }
+
  }
